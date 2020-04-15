e3d3fbebbfd121f3d7e9f7ed00259fac945c7bde
Thomas B Homburg <thomas@madethis.dk>
2020-04-15 08:43:28 +0200

    Move to /storybook and /components

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ const path = require("path");
 const withImages = require("next-images");
 
 module.exports = withImages({
-  assetPrefix: "/app/assets", // Only works for scripts, not public folder
-  webpack: (config) => {
+  // assetPrefix: "/app/assets", // Only works for scripts, not public folder
+  webpack: config => {
     config.resolve.alias["~"] = path.resolve(__dirname);
     return config;
-  },
+  }
 });
